e3eb7b49cc5c4d8e2d001d84eec784d3447fa9c2
iamaparnad <94278699+iamaparnad@users.noreply.github.com>
2022-05-18 10:56:14 +0530

    String sorting

--- CO2/Sort/SortString.java
+++ CO2/Sort/SortString.java
@@ -1,6 +1,6 @@
 import java.util.*;
 class SortString{
-	public static void main(String[] args){
+           public static void main(String[] args){
 		int i,j;
 		Scanner s=new Scanner(System.in);
 		System.out.print("Enter the number of word:");
